f3601d59f16f48665ebc25f7eecb5ed6a21e6bba
sordinho <davi.sordi96@gmail.com>
2018-06-09 22:31:25 +0200

    interrupt working. Added file for inter process interrupt hardware. Ci siamo!

--- ARD_RASP - Interrupt/interruptTest.py
+++ ARD_RASP - Interrupt/interruptTest.py
@@ -3,18 +3,22 @@ from time import sleep
 
 import RPi.GPIO as GPIO
 
-def interruptReceived():
-	print("############ INTERRUPT ############")
+def interruptReceived(channel):
+	"""
+
+	:param channel: pin number (phisical) where we received an interrupt
+	"""
+	print("############ INTERRUPT ############   ",channel)
 
 def main():
 	while True:
 		print("Waiting for interrupt")
-		sleep(2)
+		sleep(1)
 
 
 if __name__ == "__main__":
-	GPIO.setmode(GPIO.BOARD)  # to setup the numbering of pin in raspberry
-	interrupt_pin = 2
+	GPIO.setmode(GPIO.BCM)  # to setup the numbering of pin in raspberry
+	interrupt_pin = 4
 	GPIO.setup(interrupt_pin, GPIO.IN)  # Interrupt pin
-	GPIO.add_event_detect(interrupt_pin, GPIO.RISING, callback=interruptReceived)
+	GPIO.add_event_detect(interrupt_pin, GPIO.RISING, callback=interruptReceived, bouncetime=500) # bouncetime è il tempo minimo tra un interrupt e un altro
 	main()
